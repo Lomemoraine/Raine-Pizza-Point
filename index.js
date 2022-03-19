a080f8f975b269ae1bfc69eb686c3537b061cb92
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-03-19 21:05:44 +0300

    add a click jQuery function to place order button

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
-    $("table").hide();
+    $("#tableOrder").hide();
   $("button#addOrder").hide();
   $("p#para1").hide();
   $("p#para2").hide();
+  $(".bool").hide();
+  $(".location").hide();
+  $("button#checkout").hide();
+  $("#tableSummary").hide();
+  $(".finalSummary").hide();
+
+$("button#placeOrder").click(function(){
+    let size = $('#pizzaSize').find('option:selected').val();
+    let topping = $('#topping').find('option:selected').val();
+    let crust = $('#crust').find('option:selected').val();
+    let serial = 1;
+    let total = parseInt(size) + parseInt(topping) + parseInt(crust);
+    let totalPayable = 0;
+    console.log(size);
+    console.log(topping);
+    console.log(crust);
+    console.log(total);
+    $("#tableOrder").show();
+    $("button#addOrder").show();
+    $("button#checkout").show();
+
+
+})
+
 })
