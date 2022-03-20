29b2e305555696715ce668faa38380940bdca7ae
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-03-20 08:10:46 +0300

    make totalPayable and serial global variables,add enable user to add a new order

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ $(document).ready(function(){
   $("button#checkout").hide();
   $("#tableSummary").hide();
   $(".finalSummary").hide();
-
+  let serial = 1;
+  let totalPayable=0;
 $("button#placeOrder").click(function(){
+    // declare local variables to hold values from the input
     let size = $('#pizzaSizeSelect').find('option:selected').val();
     let topping = $('#topping').find('option:selected').val();
     let crust = $('#crust').find('option:selected').val();
-    let serial = 1;
+    serial = 1;
+    totalPayable = 0;
     let total = parseInt(size) + parseInt(topping) + parseInt(crust);
-    let totalPayable = 0;
+   
     console.log(size);
     console.log(topping);
     console.log(crust);
@@ -28,6 +31,37 @@ $("button#placeOrder").click(function(){
     $("#pizzaTopping").html($('#topping').find(':selected').text() +" -> "+topping);
     $("#PizzaCrust").html($('#crust').find(':selected').text() +" -> "+crust);
     $("#total").html(total)
+    // constructor for pizza
+    function pizza(size,topping,crust,total,serialNo){
+        this.size =size,
+        this.topping=topping;
+        this.crust =crust;
+        this.total = total;
+        this.serialNo= serialNo;
+    }
+    // pizza.prototype.totalPrice = function(){
+    //     let total =parseInt (this.size) + parseInt(this.topping) + parseInt(this.crust)
+    //     console.log(total);
+    // }
+    $("button#addOrder").click(function(){
+    let size = $('#pizzaSizeSelect').find('option:selected').val();
+    let topping = $('#topping').find('option:selected').val();
+    let crust = $('#crust').find('option:selected').val();
+    let total = parseInt(size) + parseInt(topping) + parseInt(crust);
+    serial = serial+1;
+    totalPayable = totalPayable + total;
+    
+
+    let pizza2 = new pizza(size, topping, crust, total,serial);
+    // pizza2.totalPrice()
+    // console.log(pizza2)
+    var newRow = '<tr><th scope="row">' + pizza2.serialNo+ '</th><td id="pizzaSize">' + $('#pizzaSizeSelect').find(':selected').text()  + " -> " + pizza2.size + '</td><td id="pizzaTopping">' + $('#topping').find(':selected').text() + " -> " + pizza2.topping + '</td><td id="PizzaCrust">' + $('#crust').find(':selected').text() + " -> " + pizza2.crust + '</td><td id="total">' + pizza2.total + '</td></tr>'
+    $("#pizza").append(newRow);
+});
+
+//    var newRow = '<tr><th scope="row">' + newPizza.serial+ '</th><td id="pizzaSize">' + $('#pizzaSizeSelect').find(':selected').text()  + " -> " + newPizza.size + '</td><td id="pizzaTopping">' + $('#topping').find(':selected').text() + " -> " + newPizza.topping + '</td><td id="PizzaCrust">' + $('#crust').find(':selected').text() + " -> " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+
+//    $("#pizza").append(newRow);
 })
 
 })
